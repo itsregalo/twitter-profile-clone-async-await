76df5811e5befe51163df5a694f3b1c62ac89e79
itsregalo <itsregalo047@gmail.com>
2023-07-15 17:00:18 +0300

    async class to perform CRUD

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,35 @@
-class UserProfile 
+class UserProfile {
+    static async getProfile() {
+        const response = await fetch('/api/profile');
+        const profile = await response.json();
+        return profile;
+    }
+
+    static async updateProfile(data) {
+        const response = await fetch('/api/profile', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        });
+        const profile = await response.json();
+        return profile;
+    }
+
+    static async deleteProfile() {
+        const response = await fetch('/api/profile', {
+            method: 'DELETE',
+        });
+        const profile = await response.json();
+        return profile;
+    }
+
+    static async getPosts() {
+        const response = await fetch('/api/posts');
+        const posts = await response.json();
+        return posts;
+    }
+
+    
+}
